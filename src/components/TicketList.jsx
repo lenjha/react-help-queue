9cbaac4d61d3895c8e81000aa72f7cc440d5c2ed
Guest User <Guest@Epicodus-1E.local>
2018-02-04 20:13:44 -0800

    lesson plan from learnhowtoprogram

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,14 @@
+src/components/TicketList.jsx
+import React from "react";
+import Ticket from "./Ticket";
+
+function TicketList(){
+  return (
+    <Ticket
+      location="3A"
+      names="Thato and Haley"
+      issue="Firebase will not save record!"/>
+  );
+}
+
+export default TicketList;
